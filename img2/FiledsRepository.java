6b71faaf2b5f691638c22c94d652ef15b19099d1
yumyum7 <106042368+yumyum7@users.noreply.github.com>
2022-09-08 11:18:43 +0900

    Add files via upload

---
+++ img2/FiledsRepository.java
@@ -0,0 +1,10 @@
+package com.cat.img;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FiledsRepository extends JpaRepository<Image, Long> {
+	
+	//Optional<Image> findById(Long id);
+}
---
+++ img2/Image.java
@@ -0,0 +1,49 @@
+package com.cat.img;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.cat.project.entity.Project;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Image {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int imgId;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false, name = "project", referencedColumnName = "pId")
+	private Project project;
+	
+	@Column(nullable = false)
+	private String fileOriName; // 업로드할 원래 파일이름
+	
+	@Column(nullable = false)
+	private String fileUrl; // 저장할 장소 
+	
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String imgDesc;
+	
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String imgLink;
+	
+
+	
+
+	
+
+	
+
+}
